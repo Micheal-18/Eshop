9a1263360233513ccc3abcf25149b17c704468e0
Micheal-18 <michaeleleke259@gmail.com>
2025-07-06 00:30:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+// import Home from './pages/Home'
+import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
+
+// Importing styles
+import './index.css'
+
+// Main App component
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<>
+        <Navbar />
+          {/* <Home />
+          <Footer /> */}
+        </>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { FaAngleDown } from 'react-icons/fa'
+import { FiSearch } from 'react-icons/fi'
+import { FaCartShopping } from 'react-icons/fa6'
+import Darkmode from './Darkmode'
+
+const Navbar = () => {
+
+  const [click, setClick] = React.useState(false);
+  const [showCart, setShowCart] = React.useState(false);
+
+  const handleCartClick = () => {
+    setShowCart(!showCart);
+  }
+
+  const handleClick = () => {
+    setClick(!click);
+  }
+
+  return (
+    <section className='w-full bg-[var] fixed top-0 mx-auto px-4'>
+      <div className='flex justify-center items-center border-1px border-primary'>
+        <div className='flex justify-between items-center w-full max-w-7xl py-4'>
+          <div className=' flex  text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer  space-x-10 '>
+            <a className=' text-[var(--primary)] text-2xl' href="/">ESHOP</a>
+            <div className='hidden md:flex lg:flex  text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer  space-x-10 '>
+              <a href="/" >Home</a>
+              <a href="/shop" >Shop</a>
+              <a href="/about" >About</a>
+              <a href="/blog" >Blog</a>
+              <a href='' className=' flex items-center justify-center'>Quick Link< FaAngleDown className='ml-2 animate' /></a>
+            </div>
+          </div>
+
+          <div className='flex justify-center gap-3 items-center  mr-6'>
+            <div className='hidden md:flex lg:flex  cursor-pointer transition ' >
+              {click ? <input type="text" placeholder='Search' className='bee ' autoFocus onBlur={() => setClick(false)} /> : <FiSearch onClick={handleClick} className='relative w-12 text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)]' />}
+            </div>
+            <div className='relative'>
+              < FaCartShopping className='text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)]' onClick={handleCartClick} />
+
+              {showCart && (<div className='absolute top-16 right-4 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-4 w-64'>
+                <h2 className='text-lg font-semibold text-left text-[var(--primary)] mb-2'>Order Now</h2>
+                <form className='flex flex-col space-y-2'>
+                  <input type="text" placeholder='Name' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
+                  <input type="email" placeholder='Email' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
+                  <input type="text" placeholder='Address' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
+                </form>
+                <button className='mt-4 px-4 py-2 rounded-lg '>Order Now</button>
+              </div>
+            )}
+            </div>
+            <Darkmode />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar

da59a873879123f570d7acdbac15054996b25035
Micheal-18 <michaeleleke259@gmail.com>
2025-07-15 17:05:47 +0100

    nav changed and home section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   }
 
   return (
-    <section className='w-full bg-[var] relative h-5 z-50 mx-auto px-4'>
+    <section className='w-full  relative h-5 z-50 mx-auto px-4'>
       <div className='fixed w-full top-0  border-1px border-primary'>
         <div className='flex justify-between items-center w-full py-4'>
           <div className=' flex  text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer  space-x-10 '>
@@ -31,12 +31,12 @@ const Navbar = () => {
               <li className=' list-none cursor-pointer outline-none  group'>
                 <a href='' className=' flex items-center justify-center'>Quick Link< FaCaretDown className='ml-2 animate' /></a>
 
-                <div className='fixed z-50 hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 transition ease-in-out p-2 text-black'>
+                <div className='fixed z-50 hidden group-hover:block w-[200px] rounded-md bg-[var(--brandWhite)]  dark:bg-[var(--brandWhite)] shadow-md dark:bg-gray-900 transition ease-in-out p-2 text-[var(--brandIcon)] text-[var(--brandIcon)]'>
                   <ul className='space-y-2'>
-                    <li className='flex flex-col space-y-2 text-gray-500 hover:text-black dark:hover:text-white duration-700 w-full'>
-                      <a href=''>Trending Fits</a>
-                      <a href=''>Latest Fits</a>
-                      <a href=''>Top Rated</a>
+                    <li className='flex flex-col space-y-2 text-gray-500 dark:hover:text-white duration-700 w-full'>
+                      <a href='' className='hover:bg-[var(--brandIcon)] rounded-sm'>Trending Fits</a>
+                      <a href='' className='hover:bg-[var(--brandIcon)] rounded-sm'>Latest Fits</a>
+                      <a href='' className='hover:bg-[var(--brandIcon)] rounded-sm'>Top Rated</a>
                     </li>
                   </ul>
                 </div>
@@ -46,18 +46,21 @@ const Navbar = () => {
 
           <div className='flex justify-center gap-3 items-center  mr-6'>
             <div className='hidden md:flex lg:flex  cursor-pointer transition ' >
-              {click ? <input type="text" placeholder='Search' className='bee ' autoFocus onBlur={() => setClick(false)} /> : <FiSearch onClick={handleClick} className='relative w-12 text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)]' />}
+              {click ? <input type="text" placeholder='Search' className='bee ' autoFocus onBlur={() => setClick(false)}  /> : <FiSearch onClick={handleClick} className='relative w-12 text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300' />}
             </div>
             <div className='relative'>
-              < FaCartShopping className='text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)]' onClick={handleCartClick} />
-              {showCart && (<div className='fixed top-16 right-4 z-50  bg-white dark:bg-gray-800 shadow-lg rounded-lg p-4 scale:120  w-64'>
+              < FaCartShopping className='text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  cursor-pointer transition ease-in-out duration-300  rounded-full hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandWhite)]' onClick={handleCartClick} />
+              <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute -top-4 -right-3 flex items-center justify-center text-xs'>4</div>
+              {showCart && (<div className=''>
+                <div className='absolute  top-12 right-0 z-50  bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)]  shadow-lg rounded-lg p-4 scale:110  w-64'>
                 <h2 className='text-lg font-semibold text-left text-[var(--primary)] mb-2'>Order Now</h2>
-                <form className='flex z-50  flex-col space-y-2'>
+                <form className='flex z-50 text-[var(--brandIcon)] dark:text-[var(--brandIcon)]  flex-col space-y-2'>
                   <input type="text" placeholder='Name' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
                   <input type="email" placeholder='Email' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
                   <input type="text" placeholder='Address' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]' />
                 </form>
                 <button className='mt-4 px-4 py-2 rounded-lg '>Order Now</button>
+              </div>
               </div>
             )}
             </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Slider from 'react-slick';
-import img1 from "../assets/headphone.png";
-import img2 from "../assets/bgcover.png";
-import img3 from "../assets/smiley.png";
+import img1 from "../assets/headphone.png"
+import img2 from "../assets/bgcover.png"
+import img3 from "../assets/smiley.png"
 import img4 from "../assets/laptop.png"
-
+import img5 from "../assets/mp3.png"
+import img6 from "../assets/p5.png"
+import img7 from "../assets/earpi.png"
+import img8 from "../assets/waka.png"
+import img9 from "../assets/phone.png"
 
 
 const Home = () => {
@@ -48,62 +52,124 @@ const Home = () => {
 
     return (
         <>
-                <section className='w-full relative flex flex-cols justify-center items-center bg-[var(--brandWhite)] dark:bg-[var(--brandBlack)]'>
-            <div className='w-[90%] h-[85vh] mt-12 bg-gray-300 dark:bg-gray-300  shadow-lg rounded-3xl'>
-                <Slider {...settings} className='w-full h-full rounded-4xl'>
-                    {
-                        HeroData.map((item) => (
-                            <div key={item.id} className=''>
-                                <div className='grid grid-cols-1 md:grid-cols-2 overflow-hidden'>
-                                    <div className='flex flex-col justify-center items-start p-8 sm:p-8 '>
-                                        <h3 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-2xl font-semibold'>{item.subtitle}</h3>
-                                        <h1 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-3xl
+            <section className='w-full relative flex flex-cols justify-center items-center'>
+                <div className='w-[90%] h-[85vh] mt-12 bg-gray-300 dark:bg-gray-300  shadow-lg rounded-3xl'>
+                    <Slider {...settings} className='w-full h-full rounded-4xl'>
+                        {
+                            HeroData.map((item) => (
+                                <div key={item.id} className=''>
+                                    <div className='grid grid-cols-1 md:grid-cols-2 overflow-hidden'>
+                                        <div className='flex flex-col justify-center items-start p-8 sm:p-8 '>
+                                            <h3 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-2xl font-semibold'>{item.subtitle}</h3>
+                                            <h1 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-3xl
                                        sm:text-7xl max-w-full opacity-80 font-bold'>{item.title}</h1>
-                                        <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[50px] md:text-[150px] font-bold'>{item.title2}</h1>
-                                        <button className='bg-[var(--brandBlack)] dark:bg-[var(--brandWhite)] text-[var(--brandWhite)] dark:text-[var(--brandBlack)] px-6 py-2 rounded-full sm:ml-4 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Shop By Cartegory</button>
-                                    </div>
-                                    <div className='flex justify-center items-center'>
-                                        <img src={item.image} alt="hero" className='w-full h-[150px] md:h-[500px] mr-8 sm:scale-105 lg:scale-110 object-contain drop-shadow-[-8px_4px_6x_rgba(0,0,0,.4)]  ' />
+                                            <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[50px] md:text-[150px] font-bold'>{item.title2}</h1>
+                                            <button className='bg-[var(--brandBlack)] dark:bg-[var(--brandWhite)] text-[var(--brandWhite)] dark:text-[var(--brandBlack)] px-6 py-2 rounded-full sm:ml-4 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Shop By Cartegory</button>
+                                        </div>
+                                        <div className='flex justify-center items-center'>
+                                            <img src={item.image} alt="hero" className='w-full h-[150px] md:h-[500px] mr-8 sm:scale-105 lg:scale-110 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]  ' />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    }
-                </Slider>
-            </div>
-        </section>
+                            ))
+                        }
+                    </Slider>
+                </div>
+            </section>
 
-         <section className='w-full relative mt-20 flex justify-center items-center bg-[var(--brandWhite)] dark:bg-[var(--brandBlack)]'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 overflow-hidden'>
-                <div className='w-[250px] p-4 h-[250px] flex-none bg-[var(--brandIcon)] border rounded-md'>
-                    <div className='flex  justify-between items-center h-full'>
-                        <div>
-                            <h1 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-2xl font-semibold'>Gadgets</h1>
-                        <button className='rounded-md p-2 '>Buy now</button>
-                        </div>
-                        <img src={img2} alt='secpic' className='object-contain w-[100px] h-[100px]'/>
-                    </div>
-                </div>
-                <div className='w-[250px] p-4 h-[250px] flex-none bg-[var(--brandYellow)] border rounded-md'>
-                    <div className='flex flex-col gap-4 items-center h-full'>
-                        <img src={img3} alt='secpic' className='object-contain w-[100px] h-[100px]'/>
-                        <div>
-                            <h1 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-2xl font-semibold'>Gadgets</h1>
-                        <button className='rounded-md p-2 bg-[var(--brandWhite)]'>Buy now</button>
+            <section className='w-full my-5 relative flex justify-center items-center'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-10 gap-8  overflow-hidden'>
+                    <div className='flex items-end relative px-4 h-[300px] shrink-0 bg-gradient-to-br from-gray-200 to-gray-300   rounded-3xl'>
+                        <div className='mb-4'>
+                            <div className=' '>
+                                <h3 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-20 font-bold'>Earphone</h1>
+                                <span className='bg-[var(--primary)] dark:bg-[var(--brandWhite)] text-[var(--brandIcon)] dark:text-[var(--brandYellow)] px-2 cursor-pointer md:px-6 py-2 rounded-full sm:ml-4 hover:bg-[var(--brandWhite)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</span>
+                            </div>
+                            
+                                <img src={img7} alt='secpic' className='w-[200px]  absolute lg:-right-4 left-5 top-10 lg:top-[40px]  object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+                            
                         </div>
                     </div>
-                </div>
-                <div className='w-[300px] p-4 h-[250px] grow bg-[var(--primary)] border rounded-md'>
-                    <div className='flex  justify-between items-center h-full'>
-                        <div>
-                            <h1 className='text-[var(--brandBlack)] dark:text-[var(--brandWhite)] text-2xl font-semibold'>Gadgets</h1>
-                        <button className='rounded-md p-2 '>Buy now</button>
+                    <div className='flex items-end relative px-4 h-[300px] bg-gradient-to-br from-yellow-400 to-yellow-400/90 rounded-3xl'>
+                        <div className='mb-4'>
+
+                            <div className=" ">
+                                <h3 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-20 font-bold'>Gadgets</h1>
+                                <span className='bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)] cursor-pointer  text-[var(--brandIcon)] dark:text-[var(--brandBlack)] px-2 md:px-6 py-2 rounded-full sm:ml-4 hover:bg-[var(--primary)] dark:hover:bg-[var(--brandIcon)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</span>
+                            </div>
                         </div>
-                        <img src={img4} alt='secpic' className='object-contain w-[100px] h-[100px]'/>
+                        <img src={img9} alt="secpic" className="w-[200px] absolute -right-4 lg:top-[40px] sm:scale-100 lg:scale-110 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]" />
+                    </div>
+                    <div className='flex items-end col-span-2 relative py-10 pl-5  h-[300px] bg-gradient-to-br from-red-500 to-red-500/90   rounded-3xl'>
+                        <div className='mb-4'>
+                            <div className=''>
+                                <h3 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-20 font-bold'>Laptop</h1>
+                                <span className='bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)] cursor-pointer  text-[var(--brandIcon)] dark:text-[var(--brandBlack)] px-2 md:px-6 py-2 rounded-full sm:ml-4 hover:bg-[var(--brandYellow)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</span>
+                            </div>
+
+                            <img src={img4} alt='secpic' className='w-[320px] absolute -right-4 top-1/2 -translate-y-1/2 sm:scale-100 lg:scale-110 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+
+                        </div>
                     </div>
                 </div>
-         </div>
-         </section>
+            </section>
+
+            <section className='w-full my-5 relative flex justify-center items-center '>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-10 gap-8  overflow-hidden'>
+                    <div className='flex col-span-2 items-end relative py-10 pl-5  h-[300px] bg-gradient-to-br from-gray-200 to-gray-400  shrink rounded-3xl'>
+                        <div className=''>
+                            <div className='mb-4'>
+                                <h3 className='text-[var(--brandIcon)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandIcon)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-white dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-30 font-bold'>Console</h1>
+                                <button className='bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)] text-[var(--brandIcon)] dark:text-[var(--brandBlack)] px-2 md:px-6 cursor-pointer py-2 rounded-full sm:ml-4 hover:bg-[var(--brandYellow)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</button>
+                            </div>
+
+                            <img src={img6} alt='secpic' className='w-[300px] absolute -right-4 top-1/2 -translate-y-1/2 sm:scale-100 lg:scale-110 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+
+                        </div>
+                    </div>
+                    <div className='flex items-start relative px-4  md:h-[300px] bg-gradient-to-br from-green-400 to-green-400/90  rounded-3xl'>
+                        <div className='my-4'>
+                            <div className=''>
+                                <h3 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-20 font-bold'>Occulus</h1>
+                                <span className='bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)] text-[var(--brandIcon)] dark:text-[var(--brandBlack)] px-2 md:px-6 py-2 rounded-full  cursor-pointer  sm:ml-4 hover:bg-[var(--brandYellow)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</span>
+                            </div>
+                            <div className='flex'>
+                                <img src={img8} alt='secpic' className='w-[200px] absolute bottom-2 right-0 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='flex items-start relative px-4 h-[300px] bg-gradient-to-br from-blue-400 to-blue-400/90  rounded-3xl'>
+                        <div className='my-4'>
+                            <div className=''>
+                                <h3 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-lg font-semibold'>Enjoy</h3>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-2xl
+                                       sm:text-3xl opacity-80 font-bold'>With</h1>
+                                <h1 className='text-[var(--brandWhite)] dark:text-[var(--brandWhite)] text-[30px] md:text-[60px] opacity-20 font-bold'>Speakers</h1>
+                                <span className='bg-[var(--brandWhite)] dark:bg-[var(--brandWhite)] text-[var(--brandIcon)] dark:text-[var(--brandBlack)] px-2 md:px-6 py-2 rounded-full  cursor-pointer  sm:ml-4 hover:bg-[var(--brandYellow)] dark:hover:bg-[var(--brandBlack)] hover:text-[var(--brandBlack)] dark:hover:text-[var(--brandWhite)] transition-all duration-300'>Browser</span>
+                            </div>
+                            
+                                <img src={img5} alt='secpic' className='w-[200px] absolute bottom-0 right-0 object-contain drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]' />
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
